3a4a669ed318af83b1bfe5f206e3a18fb23fc0d0
Paddsladd <paddsladd@icloud.com>
2025-05-17 10:17:43 +0200

    not done but wanna work on laptop

---
+++ src/lib/About/About.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import './About.scss';
+  import PaddsladdLogo from '../../assets/Paddsladd.svg';
+</script>
+
+<div class="about">
+  <img src={PaddsladdLogo} alt="Paddsladd Logo" />
+
+  <span>
+    Hi! I'm Paddsladd. I live in Sweden and love gaming. I can code TS, JS and
+    Python. Right now I'm learning C++!
+  </span>
+</div>
---
+++ src/lib/Navbar/Navbar.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import './Navbar.scss';
+
+  type navItem = {
+    url: string;
+    icon: string;
+    text: string;
+  };
+
+  const github: navItem = {
+    icon: 'cod-github_inverted',
+    text: 'Github',
+    url: 'https://github.com/Paddsladd'
+  };
+
+  const discord: navItem = {
+    icon: 'fa-discord',
+    text: 'Discord',
+    url: 'https://discord.gg/'
+  };
+
+  const email: navItem = {
+    icon: 'fa-envelope',
+    text: 'Email',
+    url: 'mailto:info@paddsladd.com'
+  };
+</script>
+
+{#snippet navElement(item: navItem)}
+  <a href={item.url} target="_blank" rel="noopener noreferrer">
+    <i class="nf-{item.icon}"></i>
+    <span>{item.text}</span>
+  </a>
+{/snippet}
+
+<div>
+  <nav>
+    {@render navElement(github)}
+    {@render navElement(discord)}
+    {@render navElement(email)}
+  </nav>
+</div>
